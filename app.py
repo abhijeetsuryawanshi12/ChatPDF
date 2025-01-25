c038a6667425efc6ad33d2febcc9ce43b7357c03
abhijeetsuryawanshi12 <abhijeetsuryawanshi128@gmail.com>
2025-01-25 18:47:14 +0530

    all imports and api key setups

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
 import streamlit as st
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+##Setup Streamlit
+st.title("ChatPDF")
+st.write("Upload Pdf's and chat with their content")
+
+#Input the Groq API key
+api_key = st.text_input("Enter your Groq API key", type="password")
+if api_key:
+    llm=ChatGroq(groq_api_key=api_key, model_name="Gemma2-9b-It")
+    
